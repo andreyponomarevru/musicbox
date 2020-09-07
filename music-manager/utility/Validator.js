b1ed2ec85d742b186d2e00d56cf3a3640d0f9c51
Andrey Ponomarev <info@andreyponomarev.ru>
2020-09-07 09:46:31 +0300

    Refactor

--- music-manager/utility/Validator.js
+++ music-manager/utility/Validator.js
@@ -2,8 +2,8 @@ const fs = require("fs-extra");
 const ValidationError = require("./ValidationError.js");
 
 class Validator {
-  constructor(value) {
-    this._value = value;
+  constructor(schema) {
+    this._schema = schema;
     this._error = [];
   }
 
@@ -11,138 +11,52 @@ class Validator {
     return this._error;
   }
 
-  isRange(from, to) {
-    if (!(this._value >= from && this._value <= to)) {
-      const error = new ValidationError(
-        "isRange",
-        `${this._value} is not in range ${from}-${to}`,
-      );
-      this._error.push(error);
-    }
-    return this;
-  }
-
-  isLength(min, max) {
-    if (!(this._value.length >= min && this._value.length <= max)) {
-      this._error.push({
-        origin: "isRange",
-        message: `${this._value} is not length ${max}`,
-      });
-    }
-    return this;
-  }
-
-  isPositive() {
-    if (!(this._value > 0))
-      this._error.push({
-        origin: "isPositive",
-        message: `${this._value} is not positive`,
-      });
-    return this;
-  }
-
-  isArr() {
-    if (!Array.isArray(this._value))
-      this._error.push({
-        origin: "isArr",
-        message: `${this._value} is not an array`,
-      });
-    return this;
-  }
-
-  isNotEmpty() {
-    if (
-      this._value === "" ||
-      this._value === null ||
-      typeof this._value !== undefined
-    ) {
-      this._error.push({
-        origin: "isNotEmpty",
-        message: `${this._value} is empty`,
-      });
-    }
-    return this;
-  }
-
-  isDiskNo() {
-    if (
-      this._value === null ||
-      typeof this._value === "string" ||
-      typeof this._value === "number"
-    )
-      return this;
-    else {
-      this._error.push({
-        origin: "isDiskNo",
-        message: `${this._value} is incorrect disk number`,
-      });
+  isType(sourceValue, targetValue = []) {
+    for (let value of targetValue) {
+      if (value === "array") return Array.isArray(sourceValue);
+      else {
+        sourceValue = sourceValue === null ? null : typeof sourceValue;
+        return targetValue.includes(sourceValue);
+      }
     }
   }
 
-  isTrackNo() {
-    this.isDiskNo();
+  isRequired(sourceValue, targetValue) {
+    return !!sourceValue === targetValue;
   }
 
-  isFloat() {
-    if (isNaN(Number.parseFloat(this._value))) {
-      this._error.push({
-        origin: "isFloat",
-        message: `${this._value} is not a float`,
-      });
-    }
-    return this;
+  isLength(sourceValue, { min, max }) {
+    return sourceValue.length >= min && sourceValue.length <= max;
   }
 
-  isInt() {
-    if (!Number.isSafeInteger(this._value)) {
-      this._error.push({
-        origin: "isInt",
-        message: `${this._value} is not an integer`,
-      });
-    }
-    return this;
+  isRange(sourceValue, { min, max }) {
+    return sourceValue >= min && sourceValue <= max;
   }
 
-  isStr() {
-    if (typeof this._value !== "string") {
-      this._error.push({
-        origin: "isString",
-        message: `${this._value} is not a string`,
-      });
-    }
-    return this;
+  isNotEmpty(sourceValue) {
+    return (
+      sourceValue !== "" && sourceValue !== null && sourceValue !== undefined
+    );
   }
 
-  isYear() {
-    const re = /^([0-9]){4}$/;
-    if (!(this._value > 0 && re.test(this._value))) {
-      this.error.push({
-        origin: "isYear",
-        message: `${this._value} is incorrect year`,
-      });
-    }
-    return this;
+  isFloat(sourceValue, targetValue) {
+    return !!Number.parseFloat(sourceValue) === targetValue;
   }
 
-  isSupportedValue(supportedValues) {
-    if (!supportedValues.includes(this._value)) {
-      this._error.push({
-        origin: "isSupportedValue",
-        message: `${supportedValues} is not supported value`,
-      });
-    }
-    return this;
+  includes(sourceValue, targetValue) {
+    return targetValue.includes(sourceValue);
   }
 
-  static setSchema(schema) {
-    this._schema = schema;
-
-    return new Validator();
-  }
-
-  async validate(props) {
-    for (let prop in props) {
-      console.log(prop);
+  async validate(dataForValidation) {
+    for (let key in dataForValidation) {
+      const sourceValue = dataForValidation[key];
+      //this[rule](validValue, this._schema[key]);
+      for (let validationRule in this._schema[key]) {
+        const targetValue = this._schema[key][validationRule];
+        console.log(this[validationRule](sourceValue, targetValue));
+        //if (!targetValue)
+        //  this._error.push(new ValidationError(dataForValidation.filePath, ``));
+      }
     }
   }
 }
