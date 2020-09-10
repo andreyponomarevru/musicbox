e7a3e2a4486e189b882c3babfd261e14608a691f
Andrey Ponomarev <info@andreyponomarev.ru>
2020-09-10 11:31:58 +0300

    postgres hangs

--- music-manager/model/track/postgres.js
+++ music-manager/model/track/postgres.js
@@ -2,47 +2,71 @@ const pg = require("pg");
 const logger = require("../../utility/loggerConf.js");
 const Track = require("./TrackModel.js");
 const validate = require("./validate.js");
+const util = require("util");
 
+const {
+  POSTGRES_USER: user,
+  POSTGRES_PASSWORD: host,
+  POSTGRES_HOST: database,
+  POSTGRES_DATABASE: password,
+  POSTGRES_PORT: port,
+} = process.env;
 let pool;
 
 async function connectDB() {
   if (pool) {
-    //logger.info("Existing pool used");
     return pool;
   } else {
-    pool = new pg.Pool();
-    //logger.info("New pool created");
+    pool = new pg.Pool({ user, host, database, password, port });
     return pool;
   }
 }
 
 async function create(metadata) {
   const pool = await connectDB();
-  /*
-  //const validatedData = await validate(metadata);
-  const track = new Track(validatedData);
-  const {
-    file_path,
-    extension,
-    artist,
-    duration,
-    bitrate,
-    year,
-    track_no,
-    title,
-    album,
-    disk_no,
-    label,
-    genre,
-    bpm,
-  } = track;
-  logger.debug("postgres.js:");
-  logger.debug(track);
-*/
+  const client = await pool.connect();
+  console.log(pool, "!!!!!!!!!!!");
+  try {
+    //const validatedData = await validate(metadata);
+    //const track = new Track(validatedData);
 
-  //await pool.query("INSERT INTO artist (name) VALUES ($1) ON CONFLICT DO NOTHING RETURNING artist_id", [ artist ]);
-  // logger.debug(`postgres.js - create():${track}`);
-  //return track;
+    console.log("!");
+    await client.query("BEGIN");
+    const queryText =
+      "WITH \
+        input_rows (name) AS ( \
+          VALUES ($1) \
+        ), \
+        \
+        ins AS ( \
+          INSERT INTO artist (name) \
+          SELECT name FROM input_rows \
+          ON CONFLICT DO NOTHING \
+          RETURNING artist_id \
+        ) \
+        \
+      SELECT artist_id FROM ins \
+      \
+      UNION ALL \
+      \
+      SELECT a.artist_id FROM input_rows \
+        JOIN artist AS a \
+       USING (name);";
+
+    const { artist_id } = (
+      await client.query(queryText, [track.artist])
+    ).rows[0];
+    console.dir(artist_id);
+
+    await client.query("COMMIT");
+    return track;
+  } catch (err) {
+    await client.query("ROLLBACK");
+    logger.error(err);
+    throw err;
+  } finally {
+    client.release();
+  }
 }
 
 async function update(location) {}
