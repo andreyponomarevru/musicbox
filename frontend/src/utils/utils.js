c89ee2c29e89ea7ae99fff9c23e7155fc6a0476d
Andrey Ponomarev <info@andreyponomarev.ru>
2020-09-13 07:03:31 +0300

    Remove irrelevant files and folders

--- frontend/src/utils/utils.js
+++
@@ -1,3 +0,0 @@
-/*
-You’ll probably end up with miscellaneous utility functions – error handlers, formatters, and the like. I usually put them in a file inside utils so I can access them easily.
-*/
