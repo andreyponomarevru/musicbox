7f384a969e40e7050b0c6c9699e851d07e636a08
Andrey Ponomarev <info@andreyponomarev.ru>
2020-10-04 00:42:00 +0300

    Import env vars from client.env instead of package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,68 +1,79 @@
 const path = require("path");
 const webpack = require("webpack");
-require('dotenv').config({ path: __dirname + "/client.env" });
-
+const dotenv = require('dotenv').config({ path: __dirname + "/client.env" });
 const { API_URL, PORT, NODE_ENV } = process.env;
 
-console.log(API_URL, PORT, NODE_ENV);
+//
+// Loaders
+//
+
+const jsLoader = {
+  test: /\.(js|jsx)$/,
+  exclude: /(node_modules|bower_components)/,
+  loader: "babel-loader",
+  options: {
+    presets: ["@babel/env", "@babel/preset-react"],
+    plugins: [
+      // "react-hot-loader/babel",
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-private-methods"
+    ]
+  }
+};
+
+const scssLoader = {
+  test: /\.scss$/,
+  loaders: [
+    require.resolve("style-loader"),
+    require.resolve("css-loader"),
+    require.resolve("sass-loader")
+  ]
+};
+
+const imageLoader = {
+  test: /\.(jpe?g|png|gif|ico|svg)$/,
+  use: [
+    {
+      loader: "file-loader",
+      options: {
+        name: "[name].[ext]",
+        context: "src",
+
+        outputPath: (url, resourcePath, context) => {
+          return `img/${url}`;
+        }
+      }
+    }
+  ]
+};
+
+const fontLoader = {
+  test: /\.(woff|woff2|eot|ttf)$/,
+  use: [
+    {
+      loader: "file-loader"
+    }
+  ]
+};
+
+//
+// Plugins
+//
+
+const dotenvPlugin = new webpack.DefinePlugin({
+  "process.env": dotenv.parsed
+});
+
+//
+// Webpack Configuration
+//
 
 module.exports = {
   entry: "./src/index.js",
   mode: NODE_ENV,
   watch: true,
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/env", "@babel/preset-react"],
-          plugins: [
-            // "react-hot-loader/babel",
-            "@babel/plugin-proposal-class-properties",
-            "@babel/plugin-proposal-private-methods"
-          ]
-        }
-      },
-
-      {
-        test: /\.scss$/,
-        loaders: [
-          require.resolve("style-loader"),
-          require.resolve("css-loader"),
-          require.resolve("sass-loader")
-        ]
-      },
-
-      {
-        test: /\.(jpe?g|png|gif|ico|svg)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              context: "src",
-
-              outputPath: (url, resourcePath, context) => {
-                return `img/${url}`;
-              }
-            }
-          }
-        ]
-      },
-
-      {
-        test: /\.(woff|woff2|eot|ttf)$/,
-        use: [
-          {
-            loader: "file-loader"
-          }
-        ]
-      }
-    ]
-  },
-
+  module: { rules: [ jsLoader, scssLoader, imageLoader, fontLoader ] },
+  plugins: [ dotenvPlugin ],
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
