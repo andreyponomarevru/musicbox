2ef1d308615dfccd4717b4f80b87a502ba9cf67c
Andrey Ponomarev <info@andreyponomarev.ru>
2020-11-10 03:54:04 +0300

    Update types

--- api-server/src/types.ts
+++ api-server/src/types.ts
@@ -1,28 +1,29 @@
 import mm from "music-metadata";
-import { validationSchema } from "model/track/validationSchema";
 
+export type TrackId = number;
 export type FilePath = string;
-export type Extension = string | null;
-export type Artist = string[] | [];
+export type Extension = string | "Unknown";
+export type Artist = string[] | ["Unknown"];
 export type Duration = number | null;
 export type Bitrate = number | null;
-export type Year = number | null;
+export type Year = number;
 export type TrackNo = number | null;
 export type Title = string | null;
 export type Album = string | null;
 export type DiskNo = number | null;
-export type Label = string | null;
-export type Genre = string[] | [];
+export type Label = string | "Unknown";
+export type Genre = string[] | ["Unknown"];
+export type PicturePath = string;
 
-export type MinMax = {
-  min: number;
-  max: number;
-};
-
-export type Includes = (string | number)[];
+export interface ExtendedIAudioMetadata extends mm.IAudioMetadata {
+  filePath: string;
+  picturePath: string | null;
+}
 
 export interface TrackMetadata {
+  trackId?: TrackId;
   filePath: FilePath;
+  picturePath: PicturePath;
   extension: Extension;
   artist: Artist;
   duration: Duration;
@@ -36,14 +37,22 @@ export interface TrackMetadata {
   genre: Genre;
 }
 
+export interface TrackPicture {
+  picturePath: string;
+  data: Buffer;
+}
+
+export type MinMax = {
+  min: number;
+  max: number;
+};
+
+export type Includes = (string | number)[];
+
 export interface ValidationSchema {
   isLength?: ((sourceValue: string, target: MinMax) => boolean) | MinMax;
   isRange?: ((sourceValue: number, target: MinMax) => boolean) | MinMax;
   includes?:
     | ((sourceValue: string | number, target: Includes) => boolean)
     | Includes;
-}
-
-export interface RawMetadata extends mm.IAudioMetadata {
-  filePath: string;
 }
