88393ebcef7d498fbf38046d1b2fe6b26901d64b
Andrey Ponomarev <info@andreyponomarev.ru>
2020-11-19 14:48:01 +0300

    Update

--- api-server/src/utility/ValidationError.ts
+++
@@ -1,8 +0,0 @@
-export class ValidationError extends Error {
-  origin: string;
-
-  constructor(origin: string, message: string) {
-    super(message);
-    this.origin = origin;
-  }
-}
--- api-server/src/utility/getExtensionName.ts
+++
@@ -1,7 +0,0 @@
-import path from "path";
-
-function getExtensionName(nodePath: string): string {
-  return path.extname(nodePath).slice(1).toLowerCase();
-}
-
-export { getExtensionName };
