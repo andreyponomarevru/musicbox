44592fff12d616303f6da16436d2ecfc8b37e2e9
Andrey Ponomarev <info@andreyponomarev.ru>
2020-11-21 16:50:40 +0300

    :rename function

--- api/src/controller/stats.ts
+++ api/src/controller/stats.ts
@@ -7,7 +7,7 @@ import * as stats from "../model/stats/queries";
 
 const router = express.Router();
 
-async function handleRoute(req: Request, res: Response, next: NextFunction) {
+async function getStats(req: Request, res: Response, next: NextFunction) {
   try {
     const retrievedStats = await stats.readLibStats();
     logger.debug(`${__filename}: ${util.inspect(retrievedStats)}`);
@@ -17,6 +17,54 @@ async function handleRoute(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-router.get("/", handleRoute);
+router.get("/", getStats);
+
+async function getGenreStats(req: Request, res: Response, next: NextFunction) {
+  try {
+    const genreStats = await stats.readGenreStats();
+    logger.debug(`${__filename}: ${util.inspect(genreStats)}`);
+    res.json(genreStats);
+  } catch (err) {
+    next(err);
+  }
+}
+
+router.get("/genres", getGenreStats);
+
+async function getYearStats(req: Request, res: Response, next: NextFunction) {
+  try {
+    const yearStats = await stats.readYearStats();
+    logger.debug(`${__filename}: ${util.inspect(yearStats)}`);
+    res.json(yearStats);
+  } catch (err) {
+    next(err);
+  }
+}
+
+router.get("/years", getYearStats);
+
+async function getArtistStats(req: Request, res: Response, next: NextFunction) {
+  try {
+    const artistStats = await stats.readArtistStats();
+    logger.debug(`${__filename}: ${util.inspect(artistStats)}`);
+    res.json(artistStats);
+  } catch (err) {
+    next(err);
+  }
+}
+
+router.get("/artists", getArtistStats);
+
+async function getLabelStats(req: Request, res: Response, next: NextFunction) {
+  try {
+    const labelStats = await stats.readLabelStats();
+    logger.debug(`${__filename}: ${util.inspect(labelStats)}`);
+    res.json(labelStats);
+  } catch (err) {
+    next(err);
+  }
+}
+
+router.get("/labels", getLabelStats);
 
 export { router };
--- api/src/model/stats/queries.ts
+++ api/src/model/stats/queries.ts
@@ -1,18 +1,69 @@
 import { logger } from "../../config/loggerConf";
 import { connectDB } from "../postgres";
 
-export async function readLibStats(): Promise<{
+type LibStats = {
   stats: {
     [key: string]: string;
   };
-}> {
+};
+
+export async function readLibStats(): Promise<LibStats> {
   const pool = await connectDB();
   try {
-    const readLibStatisticTextQuery = { text: "SELECT * FROM view_stats" };
-    const { rows } = await pool.query(readLibStatisticTextQuery);
+    const readLibStatsTextQuery = { text: "SELECT * FROM view_stats;" };
+    const { rows } = await pool.query(readLibStatsTextQuery);
+    console.log(rows[0]);
     return { stats: rows[0] };
   } catch (err) {
     logger.error(`Can't read library stats: ${err.stack}`);
+    throw err;
+  }
+}
+
+export async function readGenreStats() {
+  const pool = await connectDB();
+  try {
+    const readGenreStats = { text: "SELECT * FROM view_genre_stats;" };
+    const { rows } = await pool.query(readGenreStats);
+    return { stats: rows };
+  } catch (err) {
+    logger.error(`Can't read genre stats: ${err.stack}`);
+    throw err;
+  }
+}
+
+export async function readYearStats() {
+  const pool = await connectDB();
+  try {
+    const readYearStats = { text: "SELECT * FROM view_year_stats;" };
+    const { rows } = await pool.query(readYearStats);
+    return { stats: rows };
+  } catch (err) {
+    logger.error(`Can't read year stats: ${err.stack}`);
+    throw err;
+  }
+}
+
+export async function readArtistStats() {
+  const pool = await connectDB();
+  try {
+    const readArtistStats = { text: "SELECT * FROM view_artist_stats;" };
+    const { rows } = await pool.query(readArtistStats);
+    return { stats: rows };
+  } catch (err) {
+    logger.error(`Can't read artist stats: ${err.stack}`);
+    throw err;
+  }
+}
+
+export async function readLabelStats() {
+  const pool = await connectDB();
+  try {
+    const readLabelStats = { text: "SELECT * FROM view_label_stats;" };
+    const { rows } = await pool.query(readLabelStats);
+    return { stats: rows };
+  } catch (err) {
+    logger.error(`Can't read label stats: ${err.stack}`);
     throw err;
   }
 }
