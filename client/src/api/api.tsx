32ec44bb83e50dfed67d5212256e8e5f6dce3634
Andrey Ponomarev <info@andreyponomarev.ru>
2021-06-14 20:50:34 +0300

    [fix] add React hooks validation and update eslint rules

--- client/src/api/api.tsx
+++ client/src/api/api.tsx
@@ -1,4 +1,92 @@
-/*
-You’ll probably need to make calls to a backend API at some point. Put all that code here. If it gets too unwieldy in one file, make a src/api directory and put the area-specific API files under there – like userApi.js, productApi.js, etc.
+import {
+  PaginatedAPIResponse,
+  ReleaseMetadata,
+  TrackExtendedMetadata,
+  APIResponse,
+  DatabaseStats,
+  Stats,
+} from "../types";
 
-*/
+const { REACT_APP_API_ROOT } = process.env;
+
+//
+
+type ReleasesResponse = Promise<PaginatedAPIResponse<ReleaseMetadata[]>>;
+
+export async function getReleases(
+  sort: string,
+  limit: number,
+  page: number
+): ReleasesResponse {
+  const apiUrl = `${REACT_APP_API_ROOT}/releases?sort=${sort}&page=${page}&limit=${limit}`;
+
+  const res: PaginatedAPIResponse<ReleaseMetadata[]> = await (
+    await fetch(apiUrl)
+  ).json();
+
+  return res;
+}
+
+type TracksResponse = Promise<PaginatedAPIResponse<TrackExtendedMetadata[]>>;
+
+export async function getTracks(
+  sort: string,
+  limit: number,
+  page: number
+): TracksResponse {
+  const apiUrl = `${REACT_APP_API_ROOT}/tracks?sort=${sort}&page=${page}&limit=${limit}`;
+
+  const res: PaginatedAPIResponse<TrackExtendedMetadata[]> = await (
+    await fetch(apiUrl)
+  ).json();
+
+  return res;
+}
+
+type StatsResponse = Promise<APIResponse<DatabaseStats>>;
+
+export async function getStats(): StatsResponse {
+  const apiUrl = `${REACT_APP_API_ROOT}/stats`;
+  const res: APIResponse<DatabaseStats> = await (await fetch(apiUrl)).json();
+  return res;
+}
+
+type YearsResponse = Promise<APIResponse<Stats[]>>;
+
+export async function getYears(): YearsResponse {
+  const apiUrl = `${REACT_APP_API_ROOT}/stats/years`;
+  const res: APIResponse<Stats[]> = await (await fetch(apiUrl)).json();
+  return res;
+}
+
+type GenresResponse = Promise<APIResponse<Stats[]>>;
+
+export async function getGenres(): GenresResponse {
+  const apiUrl = `${REACT_APP_API_ROOT}/stats/genres`;
+  const res: APIResponse<Stats[]> = await (await fetch(apiUrl)).json();
+  return res;
+}
+
+type ArtistsResponse = Promise<APIResponse<Stats[]>>;
+
+export async function getArtists(): ArtistsResponse {
+  const apiUrl = `${REACT_APP_API_ROOT}/stats/artists`;
+  const res: APIResponse<Stats[]> = await (await fetch(apiUrl)).json();
+  return res;
+}
+
+type LabelsResponse = Promise<APIResponse<Stats[]>>;
+
+export async function getLabels(): LabelsResponse {
+  const apiUrl = `${REACT_APP_API_ROOT}/stats/labels`;
+  const res: APIResponse<Stats[]> = await (await fetch(apiUrl)).json();
+  return res;
+}
+
+export async function getSearchedTracks(input: string): TracksResponse {
+  const apiUrl = `${REACT_APP_API_ROOT}/search?q=${input}&page=1&limit=50`;
+  const res: PaginatedAPIResponse<TrackExtendedMetadata[]> = await (
+    await fetch(apiUrl)
+  ).json();
+  return res;
+}
