f2eb9df646ddf7ee6e0c183b33ff4e613d36c755
Andrey Ponomarev <info@andreyponomarev.ru>
2021-06-21 02:49:06 +0300

    [refactor] Move player to custom hook

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -7,7 +7,7 @@ import "./app.scss";
 import { Main } from "../page_main/main";
 import { AddRelease } from "../page_add-release/add-release";
 import { Player } from "../player/player";
-import { useFetch } from "../../state/useFetch";
+import { usePlayer } from "../../state/usePlayer";
 
 const { REACT_APP_API_ROOT } = process.env;
 let timerId: NodeJS.Timeout;
@@ -17,62 +17,36 @@ let timerId: NodeJS.Timeout;
 export function App(): ReactElement {
   // Filters
   //const [filters, setFilters] = useState<null | string[]>(null);
-
   const [searchQuery, setSearchQuery] = useState("");
-
   const [layout, setLayout] = useState<Layout>("grid");
-
-  // Player
-  // TODO: group in one object
-  // Add event lsitener to "end" event to start playing a new https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [audio, setAudio] = useState<null | HTMLAudioElement>(null);
-  const [playingTrack, setPlayingTrack] = useState<
-    undefined | TrackExtendedMetadata
-  >(undefined);
-
-  //
+  const [{ playingTrack, isPlaying, audio }, dispatch] = usePlayer();
 
   function togglePlay(selectedTrack: TrackExtendedMetadata) {
-    const audioURL = `${REACT_APP_API_ROOT}/tracks/${selectedTrack.trackId}/stream`;
+    const audioURL = `${REACT_APP_API_ROOT}/tracks/${selectedTrack?.trackId}/stream`;
 
     // If we're starting player for the first time
     if (!playingTrack) {
-      setPlayingTrack(selectedTrack);
-      setAudio(new Audio(audioURL));
-      setIsPlaying(true);
+      dispatch({
+        type: "PLAY_NEW_TRACK",
+        payload: { playingTrack: selectedTrack, audio: new Audio(audioURL) },
+      });
       // If we've clicked on an already playing tracks
-    } else if (playingTrack.trackId === selectedTrack.trackId && isPlaying) {
-      setIsPlaying(false);
+    } else if (playingTrack.trackId === selectedTrack?.trackId && isPlaying) {
+      dispatch({ type: "PAUSE" });
       // If we've clicked on a paused tracks
-    } else if (playingTrack.trackId === selectedTrack.trackId && !isPlaying) {
-      setIsPlaying(true);
+    } else if (playingTrack.trackId === selectedTrack?.trackId && !isPlaying) {
+      dispatch({ type: "RESUME" });
       // If we've clicked on a new track
-    } else if (playingTrack.trackId !== selectedTrack.trackId) {
+    } else if (playingTrack.trackId !== selectedTrack?.trackId) {
       if (audio) audio.pause();
-      setPlayingTrack(selectedTrack);
-      setAudio(new Audio(audioURL));
-      setIsPlaying(true);
+      dispatch({
+        type: "PLAY_NEW_TRACK",
+        payload: { playingTrack: selectedTrack, audio: new Audio(audioURL) },
+      });
     }
   }
 
-  // Player
-  useEffect(() => {
-    if (audio && isPlaying) audio.play();
-    else if (audio && !isPlaying) audio.pause();
-  }, [isPlaying, audio]);
-
-  function handleListBtnClick() {
-    setLayout("list");
-  }
-
-  function handleGridBtnClick() {
-    setLayout("grid");
-  }
-
   function handleSearchInput(input: string) {
-    console.log("<App> TRIGGERED");
-
     if (timerId) clearTimeout(timerId);
 
     if (input.length > 1) {
@@ -96,16 +70,16 @@ export function App(): ReactElement {
           <Main
             layout={layout}
             searchQuery={searchQuery}
-            handleListBtnClick={handleListBtnClick}
-            handleGridBtnClick={handleGridBtnClick}
-            togglePlay={togglePlay}
-            playingTrackId={playingTrack ? playingTrack.trackId : undefined}
+            handleListBtnClick={() => setLayout("list")}
+            handleGridBtnClick={() => setLayout("grid")}
+            setTrack={togglePlay}
+            playingTrackId={playingTrack?.trackId}
           />
         )}
       />
 
       <Player
-        handleTogglePlay={togglePlay}
+        setTrack={togglePlay}
         playingTrack={playingTrack}
         active={!!isPlaying}
       />
