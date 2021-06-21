9a7b9bf06e88706043e5de5392fb7b9f38dde0b6
Andrey Ponomarev <info@andreyponomarev.ru>
2021-06-21 03:07:10 +0300

    [style] remove comments (moved to tasks in Trello)

--- music-api/src/controller/search.ts
+++ music-api/src/controller/search.ts
@@ -7,24 +7,11 @@ import { parseSortParams } from "../controller/middlewares/parse-sort-params";
 
 const router = express.Router();
 
-// TODO: implement filtering
-// All queries should be case Insensitive!!!
-//
-// Minimum query: "/search?q=aya&type=all" (all = release, artist, label;)
-// Maximum query: "/search?q=aya&type=label&year=2004&genre=downtempo&genre=deep+house&year=2001&label=naked+music+recordings"
-// Type can be "all", "release", "artist", "label"
-// So, each click on filet on client side adds to the query the string like "&year=2004" i.e. "&[filterName]=[filterValue]"
-//
-// Working tested query that you can use for filtering:
-// select * from tyear where tyear in (2001, 2005, 200);
-// Read how to implement and take code form here: https://stackoverflow.com/questions/10720420/node-postgres-how-to-execute-where-col-in-dynamic-value-list-query
-
-// Filters; year, genre, label, artist
-//    ?year=2018&year=9&genre=25&label=25&artist=16
-
 async function search(req: Request, res: Response, next: NextFunction) {
   try {
     const query = String(req.query.q).toLowerCase();
+
+    console.log(`Query is: ${query}`);
 
     const params = {
       ...res.locals.sortParams,
@@ -32,10 +19,6 @@ async function search(req: Request, res: Response, next: NextFunction) {
     };
     res.locals.linkName = "tracks";
     res.locals.collection = await queriesForTrackDB.find(query, params);
-    /*
-    res.json({
-      results: matchingTracks,
-		});*/
 
     next();
   } catch (err) {
