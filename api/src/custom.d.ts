99d87f7957f557743931e81e048aa4957270c8da
Andrey Ponomarev <info@andreyponomarev.ru>
2021-08-20 08:47:56 +0300

    Delete empty file

--- api/src/custom.d.ts
+++
@@ -1,5 +0,0 @@
-import { Pagination } from "./controller/middlewares/parse-pagination-params";
-import { Sort } from "./controller/middlewares/parse-sort-params";
-import { TrackExtended } from "./model/public/track/track-extended";
-import { Track } from "./model/public/track/track";
-import { Release } from "./model/public/release/release";
